08ed1a199fdb526fa077bd8880479cb955adf684
palrang22 <92323612+palrang22@users.noreply.github.com>
2023-10-12 17:30:22 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -8,17 +8,6 @@ for n in range(N):
 
 for m in range(M):
     i, j = map(int, input().split())
-    range_list = []
-    for m in range(i-1, j):
-        range_list.append(lst[m])
-        
-    reversed_list = list(reversed(range_list))
+    lst[i-1 : j] = lst[i-1 : j][::-1]
 
-    ii = 0
-
-    for o in range(i-1, j):
-        lst[o] = reversed_list[ii]
-        ii += 1
-        
-for i in lst:
-    print(i, end = ' ')
+print(*lst)
