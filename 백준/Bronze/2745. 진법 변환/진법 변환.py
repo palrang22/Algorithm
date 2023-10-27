74e92d1d036fa37e3ad03c4c7cae1d55d202ec5f
palrang22 <92323612+palrang22@users.noreply.github.com>
2023-10-27 13:11:15 +0900

    [Bronze II] Title: 진법 변환, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.py
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -1,15 +1,2 @@
-li = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-sum = 0
-ind = 0
-
-N, B = map(str, input().split())
-B = int(B)
-N = list(N)
-N.reverse()
-
-for i in N:
-    each = (B ** ind) * (i:=li.index(i))
-    sum += each
-    ind += 1
-
-print(sum)
+N, B = input().split()
+print(int(N,int(B)))
