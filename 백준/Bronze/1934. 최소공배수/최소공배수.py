08fb9e06407879850eeaa79e72a2486d52b4604e
palrang22 <92323612+palrang22@users.noreply.github.com>
2023-11-21 15:09:54 +0900

    [Bronze I] Title: 최소공배수, Time: 128 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -0,0 +1,20 @@
+def gcd(a,b):
+    x = max([a,b])
+    y = min([a,b])
+
+    while True:
+        z = y
+        y = x%y
+        x = z
+        if y == 0:
+            return z
+        
+def lcm(c,d):
+    lcm = c * d // gcd(c,d)
+    print(lcm)
+    return lcm
+    
+N = int(input())
+for _ in range(N):
+    a, b = map(int, input().split())
+    lcm(a,b)
