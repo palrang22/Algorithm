263191ae421bcef6f50f1b44bc64fd308f5ff27a
palrang22 <92323612+palrang22@users.noreply.github.com>
2023-12-03 23:56:24 +0900

    [Silver III] Title: 알고리즘 수업 - 병합 정렬 1, Time: 2020 ms, Memory: 90200 KB -BaekjoonHub

--- 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.py
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.py
@@ -1,45 +1,42 @@
-def merge_sort(A, p, r):
-    if(p < r and count <= K):
-        q = (p + r) // 2;
-        merge_sort(A, p , q)
-        merge_sort(A, q + 1, r)
-        merge(A, p, q, r)
+def mergeSort(A, p, r):
+    if (p<r and count <= K):
+        q = (p+r) // 2
+        mergeSort(A, p, q)
+        mergeSort(A, q+1, r)
+        merge(A,p,q,r)
         
 def merge(A, p, q, r):
     global count, result
-    i, j = p, q + 1
+    i = p ; j = q+1 ; t = 0
     tmp = []
-    
-    while i <= q and j <= r:
-        if(A[i] <= A[j]):
+    while i<=q and j<=r:
+        if A[i] <= A[j]:
             tmp.append(A[i])
             i += 1
         else:
             tmp.append(A[j])
             j += 1
-        
-    while i <= q:
+    
+    while i<=q:
         tmp.append(A[i])
         i += 1
-    
-    while j <= r:
+    while j<=r:
         tmp.append(A[j])
         j += 1
     
-    i, t = p, 0;
-    
+    i = p ; t = 0
     while i <= r:
         A[i] = tmp[t]
         count += 1
         if count == K:
             result = A[i]
-            break;
-        i += 1
+            break
         t += 1
-
+        i += 1
+        
 N, K = map(int, input().split())
 A = list(map(int, input().split()))
 count = 0
 result = -1
-merge_sort(A, 0, N - 1)
+mergeSort(A, 0, N - 1)
 print(result)
