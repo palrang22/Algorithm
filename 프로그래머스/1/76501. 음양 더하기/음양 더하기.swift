0a497bcb3685c5c6da9928cefdbe8ffc68aba5ae
palrang22 <92323612+palrang22@users.noreply.github.com>
2024-04-05 15:24:04 +0900

    [level 1] Title: 음양 더하기, Time: 0.66 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.swift
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.swift
@@ -1,17 +1,5 @@
 import Foundation
 
 func solution(_ absolutes:[Int], _ signs:[Bool]) -> Int {
-    var newarr: [Int] = []
-    var newarr2: [Int] = []
-    for i in signs {
-        if i {
-            newarr.append(1)
-        } else {
-            newarr.append(-1)
-        }
-    }
-    for n in 0..<signs.count {
-        newarr2.append(absolutes[n]*newarr[n])
-    }
-    return newarr2.reduce(0,+)
+    return (0..<absolutes.count).compactMap{signs[$0] ? absolutes[$0] : -absolutes[$0]}.reduce(0,+)
 }
