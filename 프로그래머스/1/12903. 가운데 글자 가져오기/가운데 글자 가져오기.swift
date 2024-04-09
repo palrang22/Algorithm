5869a1d57336b523f18756c0313485246aa16dcd
palrang22 <92323612+palrang22@users.noreply.github.com>
2024-04-09 15:01:40 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.04 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.swift
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.swift
@@ -1,8 +1,18 @@
+import Foundation
+
 func solution(_ s:String) -> String {
-    let strlen = s.count
-    if strlen % 2 == 0 {
-        return String(Array(s)[(strlen/2 - 1)...strlen/2])
+    let strlen : Int = s.count
+    
+    if strlen == 1 {
+        return s
+    }
+    
+    let strIdx = s.index(s.startIndex, offsetBy: (strlen / 2) - 1)
+    let strIdx2 = s.index(s.startIndex, offsetBy: (strlen / 2))
+    
+    if strlen % 2 == 0 {        
+        return String(s[strIdx...strIdx2])
     } else {
-        return String(Array(s)[strlen/2])
+        return String(s[strIdx2])
     }
 }
