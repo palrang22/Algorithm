afef1c2a007a1417518f39b3bf1d1383077b6ff4
palrang22 <92323612+palrang22@users.noreply.github.com>
2024-04-19 17:23:56 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.07 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.swift
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.swift
@@ -1,27 +1,6 @@
 import Foundation
 
-func solution(_ n:Int) -> Int {
-    return(tertodeci(ternaryrev(n)))
-}
-
-func ternaryrev(_ x: Int) -> [Int] {
-    var num = x
-    var trev: [Int] = []
-    while true {
-        trev.append(num%3)
-        num = num / 3
-        if num == 0 {
-            return trev
-        }
-    }
-}
-
-func tertodeci(_ m: [Int]) -> Int{
-    let tarnary = m
-    let cnt: Int = m.count
-    var deci: Int = 0
-    for i in 1...cnt {
-        deci += tarnary[i-1] * Int(pow(3.0, Double((cnt-i))))
-    }
-    return deci
+func solution(_ n: Int) -> Int {
+    let ternary = String(n, radix: 3)
+    return Int(String(ternary.reversed()), radix: 3)!
 }
