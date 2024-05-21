059d4361ba0219937b2ff9f4f4d4cfad0f9ecd68
palrang22 <92323612+palrang22@users.noreply.github.com>
2024-05-21 15:54:42 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.swift
+++ 프로그래머스/1/12901. 2016년/2016년.swift
@@ -1,11 +1,7 @@
 func solution(_ a:Int, _ b:Int) -> String {
     let week = ["SUN","MON","TUE","WED","THU","FRI","SAT"]
     let day = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30]
-    var today = b
-    
-    for i in 0..<(a-1) {
-        today += day[i]
-    }
+    var today = day.prefix(a-1).reduce(b, +)
     
     return week[(today+4) % 7]
 }
