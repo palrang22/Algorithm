e9726593ccb5bb3518143abe16b4188ca4bd3658
palrang22 <92323612+palrang22@users.noreply.github.com>
2024-05-28 11:43:10 +0900

    [level 1] Title: 모의고사, Time: 12.58 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.swift
+++ 프로그래머스/1/42840. 모의고사/모의고사.swift
@@ -1,61 +1,23 @@
 import Foundation
 
 func solution(_ answers:[Int]) -> [Int] {
-    let qlen = answers.count
-    var ansFst: [Int] = []
-    var ansSec: [Int] = []
-    var ansTrd: [Int] = []
-    var scoreFst = 0
-    var scoreSec = 0
-    var scoreTrd = 0
-    var returnScore: [Int] = []
+    let patterns: [[Int]] = [[1, 2, 3, 4, 5], [2, 1, 2, 3, 2, 4, 2, 5], [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
+    var scores: [Int] = [0, 0, 0]
+    var returnArr = [Int]()
     
-    while ansFst.count < qlen {
-        for i in [1, 2, 3, 4, 5] {
-            ansFst.append(i)
-        }
-    }
-    
-    while ansSec.count < qlen {
-        for i in [1, 3, 4, 5] {
-            ansSec.append(2)
-            ansSec.append(i)
-        }
-    }
-    
-    while ansTrd.count < qlen {
-        for i in [3, 1, 2, 4, 5] {
-                ansTrd.append(i)
-                ansTrd.append(i)
+    for (idx, ans) in answers.enumerated() {
+        for (patidx, pat) in patterns.enumerated() {
+            if ans == pat[idx % pat.count] {
+                scores[patidx] += 1
             }
-    }
-    
-    ansFst = Array(ansFst.prefix(qlen))
-    ansSec = Array(ansSec.prefix(qlen))
-    ansTrd = Array(ansTrd.prefix(qlen))
-    
-    for i in 0..<answers.count {
-        if ansFst[i] == answers[i] {
-            scoreFst += 1
-        }
-        if ansSec[i] == answers[i] {
-            scoreSec += 1
-        }
-        if ansTrd[i] == answers[i] {
-            scoreTrd += 1
         }
     }
     
-    let maxScore = max(scoreFst, scoreSec, scoreTrd)
-    if scoreFst == maxScore {
-        returnScore.append(1)
-    }
-    if scoreSec == maxScore {
-        returnScore.append(2)
-    }
-    if scoreTrd == maxScore {
-        returnScore.append(3)
+    for (idx,score) in scores.enumerated() {
+        if score == scores.max() {
+            returnArr.append(idx+1)
+        }
     }
     
-    return returnScore
+    return returnArr
 }
