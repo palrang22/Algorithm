954f6f71b4d2be969cdb8ac67a5c8d7bb33471d3
palrang22 <92323612+palrang22@users.noreply.github.com>
2024-06-07 14:20:46 +0900

    [level 1] Title: 옹알이 (2), Time: 1.14 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.swift
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.swift
@@ -2,34 +2,22 @@ import Foundation
 
 func solution(_ babbling: [String]) -> Int {
     let can = ["aya", "ye", "woo", "ma"]
+    var newArr = babbling
     var count = 0
-    
-    for word in babbling {
-        var tempWord = word
-        var lastBabble = ""
-        var isValid = true
-        
-        while !tempWord.isEmpty && isValid {
-            var matched = false
-            for babble in can {
-                if tempWord.hasPrefix(babble) {
-                    if lastBabble == babble {
-                        isValid = false
-                        break
-                    } else {
-                        tempWord.removeFirst(babble.count)
-                        lastBabble = babble
-                        matched = true
-                        break
-                    }
-                }
-            }
-            if !matched {
-                isValid = false
-            }
+
+    for (idx, i) in newArr.enumerated() {
+        var newWord = i
+        for (jidx, j) in can.enumerated() {
+            newWord = newWord.replacingOccurrences(of:j, with: String(jidx))
         }
-        
-        if isValid && tempWord.isEmpty {
+        newArr[idx] = newWord
+    }
+    
+    for k in newArr {
+        guard let test = Int(k) else {
+            continue
+        }
+        if !k.contains("00") && !k.contains("11") && !k.contains("22") && !k.contains("33") {
             count += 1
         }
     }
