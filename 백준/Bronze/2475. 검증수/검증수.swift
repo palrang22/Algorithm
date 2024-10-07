930845b28e9c1518b1f4ee9a75ce8dab97529b76
palrang22 <92323612+palrang22@users.noreply.github.com>
2024-10-07 19:17:23 +0900

    [Bronze V] Title: 검증수, Time: 4 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.swift
+++ 백준/Bronze/2475. 검증수/검증수.swift
@@ -1,4 +1,4 @@
-var numbers: [Int] = readLine()!.split(separator: " ").compactMap{ Int($0) }
+var numbers: [Int] = readLine()!.split(separator: " ").compactMap{ Int($0)! }
 var sum: Int = 0
 for i in numbers {
     sum += i*i
