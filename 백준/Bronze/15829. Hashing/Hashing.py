6f79ca7fd22a7848e90c4f37d0525601b7c92727
palrang22 <92323612+palrang22@users.noreply.github.com>
2024-12-23 19:07:41 +0900

    [Bronze II] Title: Hashing, Time: 40 ms, Memory: 32412 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,16 @@
+alphabet_list = [chr(i) for i in range(97, 123)]
+alphabet_dict = {}
+
+for (idx, i) in enumerate(alphabet_list):
+    alphabet_dict[i] = idx + 1
+
+N = input() #버림
+exponent = 0
+result = 0
+input_list = list(input())
+
+for i in input_list:
+    result += alphabet_dict[i] * 31**exponent
+    exponent += 1
+
+print(result)
