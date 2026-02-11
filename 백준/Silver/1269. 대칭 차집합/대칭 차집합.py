b43b06078f77cda9518444a48062fce58ae93574
palrang22 <kseunghui@gmail.com>
2026-02-12 01:24:32 +0900

    [Silver IV] Title: 대칭 차집합, Time: 240 ms, Memory: 64384 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
@@ -2,7 +2,19 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split(" "))
-lst_a = set(map(int, input().split(" ")))
-lst_b = set(map(int, input().split(" ")))
+lst_a = sorted(list(map(int, input().split(" "))))
+lst_b = sorted(list(map(int, input().split(" "))))
 
-print(len(lst_a^lst_b))
+pointer_a, pointer_b = 0, 0
+common = 0
+
+while pointer_a < n and pointer_b < m:
+    if lst_a[pointer_a] < lst_b[pointer_b]:
+        pointer_a += 1
+    elif lst_a[pointer_a] == lst_b[pointer_b]:
+        common += 1
+        pointer_a += 1
+    else:
+        pointer_b += 1
+
+print(n + m - 2*common)
