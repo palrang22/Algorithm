d05a9919fb3cf8312f204c0677469ad2fa51afda
palrang22 <kseunghui@gmail.com>
2026-03-12 15:49:49 +0900

    [Silver II] Title: 연속합, Time: 80 ms, Memory: 39096 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+lst = list(map(int, input().split()))
+dp = [0] * n
+
+dp[0] = lst[0]
+for i in range(1, n):
+    dp[i] = max(dp[i-1]+lst[i], lst[i])
+
+print(max(dp))
