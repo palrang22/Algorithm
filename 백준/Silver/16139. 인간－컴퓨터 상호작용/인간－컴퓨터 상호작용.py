8fd2cd2a318e4cdeb1ed0f3f8aa432700919b5ea
palrang22 <kseunghui@gmail.com>
2026-03-14 17:11:11 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 48 ms, Memory: 32412 KB, Score: 50 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,16 +1,20 @@
 import sys
 input = sys.stdin.readline
 
-st = input()
+st = input().rstrip()
+lenst = len(st)
 row = int(input())
+alphabet_lst = [[0] * (lenst+1) for _ in range(26)]
+
+for idx, i in enumerate(st):
+    for j in range(26):
+        alphabet_lst[j][idx+1] = alphabet_lst[j][idx]
+    alp_idx = ord(i)-97
+    alphabet_lst[alp_idx][idx+1] += 1
 
 for _ in range(row):
     word, start, end = input().split()
-    start, end = int(start), int(end)
-    count = 0
+    word, start, end = word.rstrip(), int(start), int(end)
 
-    for i in range(start, end+1):
-        if st[i] == word:
-            count += 1
-    
-    print(count)
+    alp_idx = ord(word)-97
+    print(alphabet_lst[alp_idx][end+1] - alphabet_lst[alp_idx][start])
